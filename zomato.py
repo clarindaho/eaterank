d34ceb3405ff993b2d436e0914f783612b389c10
Jason Shin <jasonshin98@gmail.com>
2018-11-11 14:36:59 -0500

    Zomato Calls work

--- zomato.py
+++ zomato.py
@@ -4,7 +4,15 @@ GEOCODING_API_KEY = "AIzaSyDS1s0wGheIZjQ-r_ZP1A6Y4YZ7tnyT_KQ"
 ZOMATO_API_KEY = "fc913e8655ebbf9484b2ff4b896b6183"
 
 GEOCODING = "https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={key}"
-ZOMATO = "https://developers.zomato.com/api/v2.1"
+ZOMATO = "https://developers.zomato.com/api/v2.1/"
+
+hdrs = {
+		"User-agent": "curl/7.43.0",
+        "Accept": "application/json",
+        "Content-Type": 'application/json',
+        "X-Zomato-API-Key": ZOMATO_API_KEY
+	}
+
 
 def toLatLong(zip):
 	url = GEOCODING.format(
@@ -17,20 +25,36 @@ def toLatLong(zip):
 	return (lat, lng)
 
 def getCusinesZip(zip):
-	hdrs = {
-		"User-agent": "curl/7.43.0",
-        "Accept": "application/json",
-        "Content-Type": 'application/json',
-        "X-Zomato-API-Key": ZOMATO_API_KEY
-	}
 	coords = toLatLong(zip)
 	lat = coords[0]
 	lng = coords[1]
-	url = ZOMATO + "cuisines?lat=" + str(lat) + "&lon=" + str(lng)
-	print(url)
-	results = requests.get(url, headers = hdrs)
-	print(results.text)
-	print(results.headers['content-type'])
-	return results
+	geocode_url = ZOMATO + "geocode?lat=" + str(lat) + "&lon=" + str(lng)
+	print(geocode_url)
+	results = requests.get(geocode_url, headers = hdrs).json()
+	cuisines = results["popularity"]["top_cuisines"]
+	cuisine_ids = []
+	cuisines_url = ZOMATO + "cuisines?lat=" + str(lat) + "&lon=" + str(lng)
+	results = requests.get(cuisines_url, headers = hdrs).json()
+	for cuisine in cuisines:
+		for c in results["cuisines"]:
+			if (c["cuisine"]["cuisine_name"] == cuisine):
+				cuisine_ids.append((cuisine, c["cuisine"]["cuisine_id"]))
+	#print(results.text)
+	#print(results.headers['content-type'])
+	return cuisine_ids
 
-print(getCusinesZip(11363))
+"""
+def getRestaurants(cuisine_ids, zip):
+	coords = toLatLong(zip)
+	lat = coords[0]
+	lng = coords[1]
+	cuisines_string = ""
+	for c in cuisine_ids:
+		cuisines_string += c[1] + ","
+	cuisines_string = cuisines_string[:len(cuisines_string) - 1]
+	print(cuisines_string)
+	search_url = ZOMATO + "search?lat=" + str(lat) + "&lon=" + str(lng) + "&"
+	return ""
+"""
+
+print(getRestaurants(getCusinesZip(11363), 11363))
