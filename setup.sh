992b60bfdbcefb2a16a83e677fc52b4dc9838964
clarindaho <19913619+clarindaho@users.noreply.github.com>
2018-11-30 02:24:43 -0500

    Edited README

---
+++ setup.sh
@@ -0,0 +1,27 @@
+!/bin/sh
+
+# Install virtual environment by running
+sudo apt-get install virtualenv
+
+# Create virtual environment named eaterank
+virtualenv eaterank
+
+# Activate virtual environment
+cd eaterank
+source bin/activate
+
+# Install flask on virtual environment
+pip install flask
+
+# Install mysql connector
+pip install mysql-connector
+
+# Install supporting libraries
+pip install requests
+pip install logger
+pip install configparser
+
+# Create database on local machine
+mysql -u root -p
+source setup.sql;
+quit;
